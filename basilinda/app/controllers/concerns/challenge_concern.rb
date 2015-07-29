fc218b5f10fa428b7927d316c0b109cbb89b548b
Andrea Coravos & Tim Cannady <pair+acoravos.timcannady@devbootcamp.com>
2015-07-28 17:39:04 -0700

    unlock challenge button creates a new user_generated_challenge

--- basilinda/app/controllers/concerns/challenge_concern.rb
+++ basilinda/app/controllers/concerns/challenge_concern.rb
@@ -2,7 +2,9 @@ module ChallengeConcern
   extend ActiveSupport::Concern
 
   def generate_challenge
-
+    rand_num = rand(RandomChallenge.count)
+    challenge = RandomChallenge.offset(rand_num).first
+    current_user.random_challenges << challenge
   end
 
 end
--- basilinda/app/controllers/users_controller.rb
+++ basilinda/app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    # @unlocked_challenges = UserRandomChallenge.where(user_id: current_user.id)
   end
 
 end
